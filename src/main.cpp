fff7d1b4c41d2bd201df021c93a990ebdf068e20
seonjo1 <tjdgus9684@gmail.com>
2024-10-23 17:07:00 +0900

    Added main.cpp: main.cpp 추가

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include "../include/glmath.h"
+
+#include <iostream>
+
+int main() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    glmath::mat4 matrix;
+    glmath::vec4 vec;
+    glmath::vec4 test = matrix * vec;
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
+}
