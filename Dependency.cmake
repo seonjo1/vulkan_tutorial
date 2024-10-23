ea94ffff6fab4e97a53e78b6552ab5a358de4d21
seonjo1 <tjdgus9684@gmail.com>
2024-10-23 17:18:07 +0900

    Update Dependency.cmake: glm 추가

--- Dependency.cmake
+++ Dependency.cmake
@@ -21,3 +21,20 @@ ExternalProject_Add(
 )
 set(DEP_LIST ${DEP_LIST} dep_glfw)
 set(DEP_LIBS ${DEP_LIBS} glfw3)
+
+# glm
+ExternalProject_Add(
+	dep_glm
+	GIT_REPOSITORY "https://github.com/g-truc/glm"
+	GIT_TAG "0.9.9.8"
+	GIT_SHALLOW 1
+	UPDATE_COMMAND ""
+	PATCH_COMMAND ""
+	CONFIGURE_COMMAND ""
+	BUILD_COMMAND ""
+	TEST_COMMAND ""
+	INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory
+		${PROJECT_BINARY_DIR}/dep_glm-prefix/src/dep_glm/glm
+		${DEP_INSTALL_DIR}/include/glm
+	)
+set(DEP_LIST ${DEP_LIST} dep_glm)
