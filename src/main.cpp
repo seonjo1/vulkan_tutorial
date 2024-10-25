2f080cd3c9f64d9ac0158bd4c544137e10203687
seonjo1 <tjdgus9684@gmail.com>
2024-10-25 15:08:35 +0900

    윈도우 객체 추가 및 프레젠테이션 큐 생성

--- src/main.cpp
+++ src/main.cpp
@@ -3,38 +3,49 @@
 
 #include <iostream>
 #include <stdexcept>
-#include <cstdlib>
 #include <vector>
+#include <cstring>
+#include <cstdlib>
 #include <optional>
+#include <set>
 
 // 검증 레이어 설정
 const std::vector<const char*> validationLayers = {
-    "VK_LAYER_KHRONOS_validation"
+	"VK_LAYER_KHRONOS_validation"
 };
 
 #ifdef NDEBUG
-    const bool enableValidationLayers = false;
+	const bool enableValidationLayers = false;
 #else
-    const bool enableValidationLayers = true;
+	const bool enableValidationLayers = true;
 #endif
 
-// 디버그 메시지 콜백 함수
-static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback( VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-    												 VkDebugUtilsMessageTypeFlagsEXT messageType,
-													 const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
-													 void* pUserData) {
-	// 메시지 내용만 출력
-    std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
-	// VK_TRUE 반환시 프로그램 종료됨
-    return VK_FALSE;
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+										const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
+	// vkCreateDebugUtilsMessengerEXT 함수의 주소를 vkGetInstanceProcAddr를 통해 동적으로 가져옴
+	PFN_vkCreateDebugUtilsMessengerEXT func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+	if (func != nullptr) {
+		return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+	} else {
+		return VK_ERROR_EXTENSION_NOT_PRESENT;
+	}
 }
 
-struct QueueFamilyIndices {
-	std::optional<uint32_t> graphicsFamily;
-
-	bool isComplete() const {
-		return graphicsFamily.has_value();
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
+	PFN_vkDestroyDebugUtilsMessengerEXT func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+	if (func != nullptr) {
+		func(instance, debugMessenger, pAllocator);
 	}
+}
+
+// 큐 패밀리 인덱스 관리 구조체
+struct QueueFamilyIndices {
+    std::optional<uint32_t> graphicsFamily;
+    std::optional<uint32_t> presentFamily;
+
+    bool isComplete() {
+        return graphicsFamily.has_value() && presentFamily.has_value();
+    }
 };
 
 class HelloTriangleApplication {
@@ -48,12 +59,17 @@ public:
 
 private:
 	GLFWwindow* window;
+
 	VkInstance instance;
 	VkDebugUtilsMessengerEXT debugMessenger;
+	VkSurfaceKHR surface;
+
 	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 	VkDevice device;
-	VkQueue graphicsQueue;
 	
+	VkQueue graphicsQueue;
+	VkQueue presentQueue;
+
 	void initWindow() {
 		glfwInit();
 
@@ -66,8 +82,32 @@ private:
 	void initVulkan() {
 		createInstance();
 		setupDebugMessenger();
+		createSurface();
 		pickPhysicalDevice();
 		createLogicalDevice();
+	}
+
+	void mainLoop() {
+	    while (!glfwWindowShouldClose(window)) {
+    	    glfwPollEvents();
+    	}
+	}
+
+	void cleanup() {
+		// 메시지 객체 파괴
+		if (enableValidationLayers) {
+			DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+		}
+		// 논리적 장치 파괴
+		vkDestroyDevice(device, nullptr);
+		// 화면 객체 파괴
+		vkDestroySurfaceKHR(instance, surface, nullptr);
+		// 인스턴스 파괴
+		vkDestroyInstance(instance, nullptr);
+		// 윈도우 파괴
+		glfwDestroyWindow(window);
+		// glfw 종료
+		glfwTerminate();
 	}
 
 	void createInstance() {
@@ -115,6 +155,157 @@ private:
 		}
 	}
 
+	//vkDebugUtilsMessengerCreateInfoEXT 구조체 내부를 채워주는 함수
+	void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
+		createInfo = {};
+		createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+		createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+									VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+									VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+		createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+								VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+								VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+		createInfo.pfnUserCallback = debugCallback;
+	}
+
+	void setupDebugMessenger() {
+		// 디버그 모드 아니면 return
+		if (!enableValidationLayers) return;
+
+		// VkDebugUtilsMessengerCreateInfoEXT 구조체 생성
+		VkDebugUtilsMessengerCreateInfoEXT createInfo{};
+		populateDebugMessengerCreateInfo(createInfo);
+
+		// 디버그 메시지 객체 생성
+		if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) {
+			throw std::runtime_error("failed to set up debug messenger!");
+		}
+	}
+	
+	void createSurface() {
+		if (glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create window surface!");
+		}
+	}
+	
+	void pickPhysicalDevice() {
+		uint32_t deviceCount = 0;
+		vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+		if (deviceCount == 0) {
+  			  throw std::runtime_error("failed to find GPUs with Vulkan support!");
+		}
+		std::vector<VkPhysicalDevice> devices(deviceCount);
+		vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+		for (const auto& device : devices) {
+			if (isDeviceSuitable(device)) {
+				physicalDevice = device;
+				break;
+			}
+		}
+
+		if (physicalDevice == VK_NULL_HANDLE) {
+			throw std::runtime_error("failed to find a suitable GPU!");
+		}
+	}	
+
+	void createLogicalDevice() {
+		// 그래픽 큐 패밀리의 인덱스를 가져옴
+		QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+		// 큐 패밀리의 인덱스들을 set으로 래핑
+		std::set<uint32_t> uniqueQueueFamilies = {indices.graphicsFamily.value(), indices.presentFamily.value()};
+
+		// 큐 생성을 위한 정보 설정 
+		std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+		// 큐 우선순위 0.0f ~ 1.0f 로 표현
+		float queuePriority = 1.0f;
+		// 큐 패밀리 별로 정보 생성
+		for (uint32_t queueFamily : uniqueQueueFamilies) {
+			VkDeviceQueueCreateInfo queueCreateInfo{};
+			queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+			queueCreateInfo.queueFamilyIndex = queueFamily;
+			queueCreateInfo.queueCount = 1;
+			queueCreateInfo.pQueuePriorities = &queuePriority;
+			queueCreateInfos.push_back(queueCreateInfo);
+		}
+
+		// 사용할 장치 기능이 포함된 구조체
+		// vkGetPhysicalDeviceFeatures 함수로 디바이스에서 설정 가능한
+		// 장치 기능 목록을 확인할 수 있음
+		// 일단 지금은 VK_FALSE로 전부 등록함
+		VkPhysicalDeviceFeatures deviceFeatures{};
+
+		// 논리적 장치 생성을 위한 정보 등록
+		VkDeviceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+		createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
+		createInfo.pQueueCreateInfos = queueCreateInfos.data();
+		createInfo.pEnabledFeatures = &deviceFeatures;
+		// 확장 수는 일단 0으로 설정
+		createInfo.enabledExtensionCount = 0;
+		// 구버전 호환을 위해 디버그 모드일 경우
+		// 검증 레이어를 포함 시키지만, 현대 시스템에서는 논리적 장치의 레이어를 안 씀
+		if (enableValidationLayers) {
+			createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+			createInfo.ppEnabledLayerNames = validationLayers.data();
+		} else {
+			createInfo.enabledLayerCount = 0;
+		}
+
+		// 논리적 장치 생성
+		if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+ 		   throw std::runtime_error("failed to create logical device!");
+		}
+
+		// 큐 핸들 가져오기
+		vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
+		vkGetDeviceQueue(device, indices.presentFamily.value(), 0, &presentQueue);
+	}
+
+	bool isDeviceSuitable(VkPhysicalDevice device) {
+		QueueFamilyIndices indices = findQueueFamilies(device);
+	    return indices.isComplete();
+	}
+
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+		QueueFamilyIndices indices;
+
+		// 지원하는 큐 패밀리 개수 가져오기
+		uint32_t queueFamilyCount = 0;
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+		// 큐 패밀리 리스트 가져오기
+		std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+		// 그래픽 큐 패밀리 검색
+		int i = 0;
+		for (const auto& queueFamily : queueFamilies) {
+			// 그래픽 큐 패밀리 찾기 성공한 경우 indices에 값 생성
+			if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+				indices.graphicsFamily = i;
+			}
+
+			// 프레젠테이션 큐 패밀리인지 확인
+            VkBool32 presentSupport = false;
+            vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &presentSupport);
+
+			// 프레젠테이션 큐 패밀리 등록
+            if (presentSupport) {
+                indices.presentFamily = i;
+            }
+
+			// 그래픽 큐 패밀리 찾은 경우 break
+			if (indices.isComplete()) {
+				break;
+			}
+
+			i++;
+		}
+		// 그래픽 큐 패밀리를 못 찾은 경우 값이 없는 채로 반환 됨
+		return indices;
+	}
+
 	std::vector<const char*> getRequiredExtensions() {
 		// 필요한 확장 목록 가져오기
 		uint32_t glfwExtensionCount = 0;
@@ -153,174 +344,15 @@ private:
 		return true;  // 모든 레이어가 지원되면 true 반환
 	}
 
-	void setupDebugMessenger() {
-		// 디버그 모드 아니면 return
-		if (!enableValidationLayers) return;
-
-		// VkDebugUtilsMessengerCreateInfoEXT 구조체 생성
-		VkDebugUtilsMessengerCreateInfoEXT createInfo{};
-		populateDebugMessengerCreateInfo(createInfo);
-
-		// 디버그 메시지 객체 생성
-		if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) {
-			throw std::runtime_error("failed to set up debug messenger!");
-		}
-	}
-
-	//vkDebugUtilsMessengerCreateInfoEXT 구조체 내부를 채워주는 함수
-	void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
-		createInfo = {};
-		createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-		createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
-									VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
-									VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-		createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
-								VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
-								VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
-		createInfo.pfnUserCallback = debugCallback;
-	}
-
-	VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
-										  const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
-		// vkCreateDebugUtilsMessengerEXT 함수의 주소를 vkGetInstanceProcAddr를 통해 동적으로 가져옴
-		PFN_vkCreateDebugUtilsMessengerEXT func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
-		if (func != nullptr) {
-			return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
-		} else {
-			return VK_ERROR_EXTENSION_NOT_PRESENT;
-		}
-	}
-
-	void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
-		PFN_vkDestroyDebugUtilsMessengerEXT func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
-		if (func != nullptr) {
-			func(instance, debugMessenger, pAllocator);
-		}
-	}
-
-	void pickPhysicalDevice() {
-		//
-		uint32_t deviceCount = 0;
-		vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
-		if (deviceCount == 0) {
-  			  throw std::runtime_error("failed to find GPUs with Vulkan support!");
-		}
-		std::vector<VkPhysicalDevice> devices(deviceCount);
-		vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
-
-		for (const auto& device : devices) {
-			if (isDeviceSuitable(device)) {
-				physicalDevice = device;
-				break;
-			}
-		}
-
-		if (physicalDevice == VK_NULL_HANDLE) {
-			throw std::runtime_error("failed to find a suitable GPU!");
-		}
-	}	
-
-	bool isDeviceSuitable(VkPhysicalDevice device) {
-		QueueFamilyIndices indices = findQueueFamilies(device);
-	    return indices.isComplete();
-	}
-
-	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
-		QueueFamilyIndices indices;
-
-		// 지원하는 큐 패밀리 개수 가져오기
-		uint32_t queueFamilyCount = 0;
-		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
-
-		// 큐 패밀리 리스트 가져오기
-		std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
-		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
-
-		// 그래픽 큐 패밀리 검색
-		int i = 0;
-		for (const auto& queueFamily : queueFamilies) {
-			// 그래픽 큐 패밀리 찾기 성공한 경우 indices에 값 생성
-			if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
-				indices.graphicsFamily = i;
-			}
-
-			// 그래픽 큐 패밀리 찾은 경우 break
-			if (indices.isComplete()) {
-				break;
-			}
-
-			i++;
-		}
-		// 그래픽 큐 패밀리를 못 찾은 경우 값이 없는 채로 반환 됨
-		return indices;
-	}
-
-	void createLogicalDevice() {
-		// 그래픽 큐 패밀리의 인덱스를 가져옴
-		QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
-		
-		// 큐 생성을 위한 정보 설정 
-		VkDeviceQueueCreateInfo queueCreateInfo{};
-		queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-		queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
-		queueCreateInfo.queueCount = 1;
-		// 큐 우선순위 0.0f ~ 1.0f 로 표현
-		// 1.0f 이 제일 우선순위가 높음
-		float queuePriority = 1.0f;
-		queueCreateInfo.pQueuePriorities = &queuePriority;
-
-		// 사용할 장치 기능이 포함된 구조체
-		// vkGetPhysicalDeviceFeatures 함수로 디바이스에서 설정 가능한
-		// 장치 기능 목록을 확인할 수 있음
-		// 일단 지금은 VK_FALSE로 전부 등록함
-		VkPhysicalDeviceFeatures deviceFeatures{};
-
-		// 논리적 장치 생성을 위한 정보 등록
-		VkDeviceCreateInfo createInfo{};
-		createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-		// 큐 패밀리를 여러 개 쓰고 싶으면 여러개의 VKDeviceQueueCreateInfo가 필요
-		createInfo.pQueueCreateInfos = &queueCreateInfo;
-		createInfo.queueCreateInfoCount = 1;
-		createInfo.pEnabledFeatures = &deviceFeatures;
-		// 확장 수는 일단 0으로 설정
-		createInfo.enabledExtensionCount = 0;
-		// 구버전 호환을 위해 디버그 모드일 경우
-		// 검증 레이어를 포함 시키지만, 현대 시스템에서는 논리적 장치의 레이어를 안 씀
-		if (enableValidationLayers) {
-			createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
-			createInfo.ppEnabledLayerNames = validationLayers.data();
-		} else {
-			createInfo.enabledLayerCount = 0;
-		}
-
-		// 논리적 장치 생성
-		if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
- 		   throw std::runtime_error("failed to create logical device!");
-		}
-
-		// 큐 핸들 가져오기
-		vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
-	}
-
-	void mainLoop() {
-	    while (!glfwWindowShouldClose(window)) {
-    	    glfwPollEvents();
-    	}
-	}
-
-	void cleanup() {
-		// 메시지 객체 파괴
-		if (enableValidationLayers) {
-			DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
-		}
-		// 논리적 장치 파괴
-		vkDestroyDevice(device, nullptr);
-		// 인스턴스 파괴
-		vkDestroyInstance(instance, nullptr);
-		// 윈도우 파괴
-		glfwDestroyWindow(window);
-		// glfw 종료
-		glfwTerminate();
+	// 디버그 메시지 콜백 함수
+	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback( VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+														VkDebugUtilsMessageTypeFlagsEXT messageType,
+														const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+														void* pUserData) {
+		// 메시지 내용만 출력
+		std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+		// VK_TRUE 반환시 프로그램 종료됨
+		return VK_FALSE;
 	}
 };
 
