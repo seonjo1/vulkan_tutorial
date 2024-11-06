541ab8295cfa4a9dba2e0cbb8cd665937629b237
seonjo1 <tjdgus9684@gmail.com>
2024-11-06 22:45:35 +0900

    Staging buffer

--- src/main.cpp
+++ src/main.cpp
@@ -856,61 +856,139 @@ private:
 
 	/*
 		[버텍스 버퍼 생성]
-		1. 버텍스 버퍼 객체 생성
-		2. 버텍스 버퍼 메모리 할당
-		3. 버텍스 버퍼 객체에 할당한 메모리 바인딩
-		4. GPU 버텍스 버퍼 메모리에 정점 정보 입력
+		1. 스테이징 버퍼 생성
+		2. 스테이징 버퍼에 정점 정보 복사
+		3. 버텍스 버퍼 생성
+		4. 스테이징 버퍼의 정점 정보를 버텍스 버퍼에 복사
+		5. 스테이징 버퍼 리소스 해제
 	*/ 
 	void createVertexBuffer() {
-		// 버퍼 객체를 생성하기 위한 구조체 (GPU 메모리에 데이터 저장 공간을 할당하는 데 필요한 설정을 정의)
-		VkBufferCreateInfo bufferInfo{};
-		bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-		bufferInfo.size = sizeof(vertices[0]) * vertices.size();	// 버퍼의 크기 지정
-		bufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;		// 버퍼의 용도 지정 (현재는 버텍스 버퍼로 지정)
-		bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;			// 버퍼를 하나의 큐 패밀리에서 쓸지, 여러 큐 패밀리에서 공유할지 설정 (현재 단일 큐 패밀리에서 사용하도록 설정) 
-																	// 여러 큐 패밀리에서 공유하는 모드 사용시 추가 설정 필요
-		// [버텍스 버퍼 생성]
-		// 버퍼를 생성하지만 할당은 안되어있는 상태로 만들어짐
-		if (vkCreateBuffer(device, &bufferInfo, nullptr, &vertexBuffer) != VK_SUCCESS) {
-			throw std::runtime_error("failed to create vertex buffer!");
-		}
 
-		// [버퍼에 메모리 할당]
-		// 메모리 할당 요구사항 조회
-		VkMemoryRequirements memRequirements;										// 메모리 할당 요구사항이 기록될 구조체
-		vkGetBufferMemoryRequirements(device, vertexBuffer, &memRequirements);		// GPU 와 buffer 정보에 따른 메모리 할당 요구사항 반환
+		// 정점 정보 크기		
+		VkDeviceSize bufferSize = sizeof(vertices[0]) * vertices.size();
 
-		// 메모리 할당을 위한 구조체
-		VkMemoryAllocateInfo allocInfo{};
-		allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-		allocInfo.allocationSize = memRequirements.size;	// 할당할 메모리 크기
-		// 메모리 요구사항 설정
-		// 메모리 유형
-		// 		typeFileter : GPU와 buffer에 호환되는 메모리 유형 모음
-		// 메모리 유형의 속성
+		// 스테이징 버퍼 객체, 스테이징 버퍼 메모리 객체 생성
+        VkBuffer stagingBuffer;
+        VkDeviceMemory stagingBufferMemory;
+
+		// [스테이징 버퍼 생성]
+		// 용도)
+		//		VK_BUFFER_USAGE_TRANSFER_SRC_BIT : 데이터 전송의 소스로 사용
+		// 속성)
 		// 		VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT  : CPU에서 GPU 메모리에 접근이 가능한 설정
 		// 		VK_MEMORY_PROPERTY_HOST_COHERENT_BIT : CPU에서 GPU 메모리의 값을 수정하면 그 즉시 GPU 메모리와 캐시에 해당 값을 수정하는 설정 
 		//      									  (원래는 CPU에서 GPU 메모리 값을 수정하면 GPU 캐시를 플러쉬하여 다시 캐시에 값을 올리는 형식으로 동작)
-		allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
-		
-		// 버텍스 버퍼 메모리 할당
-		if (vkAllocateMemory(device, &allocInfo, nullptr, &vertexBufferMemory) != VK_SUCCESS) {
-			throw std::runtime_error("failed to allocate vertex buffer memory!");
-		}
+        createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
+		// [스테이징 버퍼(GPU 메모리)에 정점 정보 입력]
+		void* data; // GPU 메모리에 매핑될 CPU 메모리 가상 포인터
+		vkMapMemory(device, stagingBufferMemory, 0, bufferSize, 0, &data); 	// GPU 메모리에 매핑된 CPU 메모리 포인터 반환 (현재는 버텍스 버퍼 메모리 전부를 매핑)
+		memcpy(data, vertices.data(), (size_t) bufferSize);					// CPU 메모리 포인터는 가상포인터로 data에 정점 정보를 복사하면 GPU에 즉시 반영
+																			// 실제 CPU 메모리에 저장되는게 아닌 CPU 메모리 포인터는 가상 포인터역할만 하고 GPU 메모리에 바로 저장
+																			// 메모리 유형의 속성에 의해 GPU 캐시를 플러쉬할 필요없이 즉시 적용
+																			// VK_MEMORY_PROPERTY_HOST_COHERENT_BIT 속성 덕분
+																			// 만약 해당 속성 없이 플러쉬도 안 하면 GPU에서 변경사항이 바로 적용되지 않음
+		vkUnmapMemory(device, stagingBufferMemory);							// GPU 메모리 매핑 해제
+
+
+		// [버텍스 버퍼 생성]
+		// 용도)
+		//		VK_BUFFER_USAGE_TRANSFER_DST_BIT : 데이터 전송의 대상으로 사용
+		// 속성)
+		// 		VK_BUFFER_USAGE_VERTEX_BUFFER_BIT : 버퍼를 정점 데이터를 저장하고 처리하는 용도로 설정.
+		// 		VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT : GPU 전용 메모리에 데이터를 저장하여, GPU가 최적화된 방식으로 접근할 수 있게 함.
+        createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, vertexBuffer, vertexBufferMemory);
+
+		// [스테이징 버퍼에서 버텍스 버퍼로 메모리 이동]
+        copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+
+		// 스테이징 버퍼와 할당된 메모리 해제
+        vkDestroyBuffer(device, stagingBuffer, nullptr);
+        vkFreeMemory(device, stagingBufferMemory, nullptr);
+	}
+
+	/*
+		[버퍼 생성]
+		1. 버퍼 객체 생성
+		2. 버퍼 메모리 할당
+		3. 버퍼 객체에 할당한 메모리 바인딩
+	*/ 
+    void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory) {
+		// 버퍼 객체를 생성하기 위한 구조체 (GPU 메모리에 데이터 저장 공간을 할당하는 데 필요한 설정을 정의)
+	    VkBufferCreateInfo bufferInfo{};
+        bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+        bufferInfo.size = size;										// 버퍼의 크기 지정
+        bufferInfo.usage = usage;									// 버퍼의 용도 지정
+        bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;			// 버퍼를 하나의 큐 패밀리에서 쓸지, 여러 큐 패밀리에서 공유할지 설정 (현재 단일 큐 패밀리에서 사용하도록 설정)
+																	// 여러 큐 패밀리에서 공유하는 모드 사용시 추가 설정 필요
+		// [버퍼 생성]
+		// 버퍼를 생성하지만 할당은 안되어있는 상태로 만들어짐       
+	    if (vkCreateBuffer(device, &bufferInfo, nullptr, &buffer) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create buffer!");
+        }
+
+		// [버퍼에 메모리 할당]
+		// 메모리 할당 요구사항 조회
+        VkMemoryRequirements memRequirements;
+        vkGetBufferMemoryRequirements(device, buffer, &memRequirements);
+
+		// 메모리 할당을 위한 구조체
+        VkMemoryAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+        allocInfo.allocationSize = memRequirements.size;	// 할당할 메모리 크기
+        // 메모리 요구사항 설정 (GPU 메모리 유형 중 buffer와 호환되고 properties 속성들과 일치하는 것 찾아 저장)
+		// 메모리 유형 - GPU 메모리는 구역마다 유형이 다르다. (memoryTypeBits는 buffer가 호환되는 GPU의 메모리 유형이 전부 담겨있음)
+		// 메모리 유형의 속성 - 메모리 유형마다 특성을 가지고 있음
+		allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, properties);
+
+		// 버퍼 메모리 할당
+        if (vkAllocateMemory(device, &allocInfo, nullptr, &bufferMemory) != VK_SUCCESS) {
+            throw std::runtime_error("failed to allocate buffer memory!");
+        }
 
 		// 버퍼 객체에 할당된 메모리를 바인딩 (4번째 매개변수는 할당할 메모리의 offset)
-		vkBindBufferMemory(device, vertexBuffer, vertexBufferMemory, 0);
+        vkBindBufferMemory(device, buffer, bufferMemory, 0);
+    }
 
-		// GPU 메모리에 정점 정보 입력
-		void* data; // GPU 메모리에 매핑될 CPU 메모리 가상 포인터
-		vkMapMemory(device, vertexBufferMemory, 0, bufferInfo.size, 0, &data); 	// GPU 메모리에 매핑된 CPU 메모리 포인터 반환 (현재는 버텍스 버퍼 메모리 전부를 매핑)
-		memcpy(data, vertices.data(), (size_t) bufferInfo.size);				// CPU 메모리 포인터는 가상포인터로 data에 정점 정보를 복사하면 GPU에 즉시 반영
-																				// 실제 CPU 메모리에 저장되는게 아닌 CPU 메모리 포인터는 가상 포인터역할만 하고 GPU 메모리에 바로 저장
-																				// 메모리 유형의 속성에 의해 GPU 캐시를 플러쉬할 필요없이 즉시 적용
-																				// VK_MEMORY_PROPERTY_HOST_COHERENT_BIT 속성 덕분
-																				// 만약 해당 속성 없이 플러쉬도 안 하면 GPU에서 변경사항이 바로 적용되지 않음
-		vkUnmapMemory(device, vertexBufferMemory);								// GPU 메모리 매핑 해제
-	}
+	// srcBuffer 에서 dstBuffer 로 데이터 복사
+    void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size) {
+        // 커맨드 버퍼 할당을 위한 구조체 
+		VkCommandBufferAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        allocInfo.commandPool = commandPool;	// 커맨드 풀 지정
+        allocInfo.commandBufferCount = 1;		// 커맨드 버퍼 개수
+
+		// 커맨드 버퍼 생성
+        VkCommandBuffer commandBuffer;
+        vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer);
+
+		// 커맨드 버퍼 기록을 위한 정보 객체
+        VkCommandBufferBeginInfo beginInfo{};
+        beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+        beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;  // 커맨드 버퍼를 1번만 제출
+
+		// GPU에 필요한 작업을 모두 커맨드 버퍼에 기록하기 시작
+        vkBeginCommandBuffer(commandBuffer, &beginInfo);
+
+		VkBufferCopy copyRegion{}; 	// 복사할 버퍼 영역을 지정 (크기, src 와 dst의 시작 offset 등)
+		copyRegion.size = size;		// 복사할 버퍼 크기 설정
+		vkCmdCopyBuffer(commandBuffer, srcBuffer, dstBuffer, 1, &copyRegion); // 커맨드 버퍼에 복사 명령 기록
+
+		// 커맨드 버퍼 기록 중지
+        vkEndCommandBuffer(commandBuffer);
+
+		// 복사 커맨드 버퍼 제출 정보 객체 생성
+        VkSubmitInfo submitInfo{};
+        submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+        submitInfo.commandBufferCount = 1;								// 커맨드 버퍼 개수
+        submitInfo.pCommandBuffers = &commandBuffer;					// 커맨드 버퍼 등록
+
+        vkQueueSubmit(graphicsQueue, 1, &submitInfo, VK_NULL_HANDLE);	// 커맨드 버퍼 큐에 제출
+        vkQueueWaitIdle(graphicsQueue);									// 그래픽스 큐 작업 종료 대기
+
+        vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);	// 커맨드 버퍼 제거
+    }
 
 	/*
 		GPU와 buffer가 호환되는 메모리 유형중 properties에 해당하는 속성들을 갖는 메모리 유형 찾기
